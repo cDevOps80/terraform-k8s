245cfebfb8f5a62bdcc57ea4c7bb8e009910734d
chaithanya1812 <bob@example.com>
2024-09-10 19:46:20 +0530

    terraform to deal with multiple aws accounts

--- 01-data-resources/main.tf
+++ 01-data-resources/main.tf
@@ -2,23 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#data "ami_id" "amazon-linux" {
-#  owners           = ["amazon"]
-#  most_recent      = true
-#
-#  filter {
-#    name   = "name"
-#    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2"]
-#  }
-#
-#  filter {
-#    name   = "architecture"
-#    values = ["x86_64"]
-#  }
-#}
-#
-#output "amazon-linux-id" {
-#  value = data.ami_id.amazon-linux.id
 
 
 data "aws_ami" "example" {
@@ -28,6 +11,8 @@ data "aws_ami" "example" {
       name   = "name"
       values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2"]
     }
+}
 
-
+output "amazon-linux-id" {
+  value = data.example
 }
