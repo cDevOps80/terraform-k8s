029db5cd88a1b250cfb3a2b677b4abccbf90eaf3
chaithanya1812 <bob@example.com>
2024-09-10 20:22:33 +0530

    terraform to deal with multiple aws accounts

--- 01-data-resources/main.tf
+++ 01-data-resources/main.tf
@@ -24,7 +24,7 @@ data "aws_ami_ids" "example" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*.0.20240903.0-*-gp2"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2"]
 
 #    amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2
 #
