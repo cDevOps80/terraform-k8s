a17819df507eecd8ea23ba6c6aa3221b5198ea6a
chaithanya1812 <bob@example.com>
2024-09-10 23:24:18 +0530

    key pair

--- advanced-loops/main.tf
+++ advanced-loops/main.tf
@@ -1,30 +1,38 @@
-resource "aws_instance" "good" {
-  for_each = var.instance_types
-  ami           = "ami-0a5c3558529277641"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "test-${each.key}"
-  }
-}
-
-variable "instance_types" {
-  default = {
-    one = "t2.micro"
-    two = "t2.small"
-  }
-}
-
-#output "final" {
-#  value = aws_instance.good
+#resource "aws_instance" "good" {
+#  for_each = var.instance_types
+#  ami           = "ami-0a5c3558529277641"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "test-${each.key}"
+#  }
+#}
+#
+#variable "instance_types" {
+#  default = {
+#    one = "t2.micro"
+#    two = "t2.small"
+#  }
+#}
+#
+##output "final" {
+##  value = aws_instance.good
+##}
+#
+#output "instance_publicdns2" {
+#  value = {
+#    for key, myec2vm in aws_instance.good : key => myec2vm.private_ip
+#  }
+#}
+#
+#output "instance_publicdns" {
+#  value = [ for key, values in aws_instance.good : key  ]
 #}
 
-output "instance_publicdns2" {
-  value = {
-    for key, myec2vm in aws_instance.good : key => myec2vm.private_ip
-  }
+data "aws_availability_zone" "example" {
+  name = "us-east-1a"
 }
 
-output "instance_publicdns" {
-  value = [ for key, values in aws_instance.good : key  ]
+output "data" {
+  value = data.aws_availability_zone.example
 }
