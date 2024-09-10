8d173c120d26105b3ad4dd2eefe419a5635fe239
chaithanya1812 <bob@example.com>
2024-09-10 20:37:19 +0530

    terraform to deal with multiple aws accounts

--- 01-data-resources/main.tf
+++ 01-data-resources/main.tf
@@ -1,38 +1,20 @@
-provider "aws" {
-  region = "us-east-1"
+data "aws_vpc" "default" {
+  default = true
 }
 
-
-
-#data "aws_ami" "example" {
-#  most_recent      = true
-#  owners           = ["amazon"]
-#    filter {
-#      name   = "name"
-#      values = ["amzn2-ami-kernel-5.10-hvm-2.*"]
-#    }
-#
-#  filter {
-#    name   = "architecture"
-#    values = ["arm64"]
-#  }
-#}
-
-data "aws_ami_ids" "example" {
-
-  owners      = ["amazon"]
-
+data "aws_key_pair" "example" {
   filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2"]
-
+    name   = "key-name"
+    values = ["nvirginia"]
   }
-
 }
 
-output "final" {
-  value = data.aws_ami_ids.example
+output "data-key" {
+  value = data.aws_key_pair.example.public_key
 }
 
-
-
+#resource "aws_instance" "one" {
+#  ami  = "ami-0a5c3558529277641"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = []
+#}
