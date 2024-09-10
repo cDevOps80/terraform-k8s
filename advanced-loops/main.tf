4f463c619dd8fc96be7397cd43f4963432131c11
chaithanya1812 <bob@example.com>
2024-09-10 23:08:42 +0530

    key pair

--- advanced-loops/main.tf
+++ advanced-loops/main.tf
@@ -20,7 +20,7 @@ output "final" {
 }
 
 output "instance_publicdns2" {
-  value = tomap({
-    for  myec2vm in aws_instance.good : myec2vm.availability_zone => myec2vm.public_dns
-  })
+  value = {
+    for key, myec2vm in aws_instance.good : key => myec2vm.private_ip
+  }
 }
