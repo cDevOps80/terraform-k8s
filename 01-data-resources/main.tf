29e830793c11e8c6414ee56269dc5cc932787278
chaithanya1812 <bob@example.com>
2024-09-10 19:39:23 +0530

    terraform to deal with multiple aws accounts

--- 01-data-resources/main.tf
+++ 01-data-resources/main.tf
@@ -5,9 +5,10 @@ provider "aws" {
 data "ami_id" "amazon-linux" {
   owners           = ["amazon"]
   most_recent      = true
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.*"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240903.0-x86_64-gp2"]
   }
 }
 
