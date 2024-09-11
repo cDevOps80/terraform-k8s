a89c6ba19b885dcc6e6274f030b2ad0a22b72adb
chaithanya1812 <bob@example.com>
2024-09-11 22:17:21 +0530

    key pair

--- utility-project/data.tf
+++ utility-project/data.tf
@@ -1,6 +1,16 @@
 data "aws_ec2_instance_type_offering" "example" {
   filter {
     name   = "instance-type"
-    values = ["t2.micro", "t3.micro"]
+    values = ["t3.micro"]
   }
+  filter {
+    name   = "location"
+    values = ["us-east-1a"]
+  }
+
+  location_type = "availability-zone"
+}
+
+output "finale" {
+  value = data.aws_ec2_instance_type_offering.example.instance_type
 }
