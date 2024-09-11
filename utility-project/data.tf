950ae2655e04d73a1000e26e9e6c7b3b9f87920e
chaithanya1812 <bob@example.com>
2024-09-11 22:26:32 +0530

    key pair

--- utility-project/data.tf
+++ utility-project/data.tf
@@ -1,5 +1,5 @@
 variable "av_zones" {
-  default = ["us-east-1a","us-east-1b","us-east-1c","us-east-1e","us-east-1f"]
+  default = ["us-east-1a","us-east-1b"]
 }
 
 data "aws_ec2_instance_type_offering" "example" {
