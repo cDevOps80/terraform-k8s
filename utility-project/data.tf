dcba468b7e4853d6b90fe8612add8f992d1b35ff
chaithanya1812 <bob@example.com>
2024-09-11 22:11:57 +0530

    key pair

--- utility-project/data.tf
+++ utility-project/data.tf
@@ -1,3 +1,9 @@
 data "external" "final" {
-  program = ["aws","ec2","describe-instance-type-offerings","--region","us-east-1","--location-type","availability-zone" ,"--filters", "Name=instance-type,Values=t3.micro","--output json"]
+  program = ["bash","${path.module}/run.sh"]
 }
+
+#resource "null_resource" "one" {
+#  provisioner "local-exec" {
+#    command = ["aws","ec2","describe-instance-type-offerings","--region","us-east-1","--location-type","availability-zone" ,"--filters", "Name=instance-type,Values=t3.micro","--output json"]
+#  }
+#}
