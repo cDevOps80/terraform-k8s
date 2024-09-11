c35d0f2ce289b3ece09b71cb1f8ad9ea2ddb291f
chaithanya1812 <bob@example.com>
2024-09-11 22:20:38 +0530

    key pair

--- utility-project/data.tf
+++ utility-project/data.tf
@@ -1,11 +1,16 @@
+variable "av_zones" {
+  default = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
+}
+
 data "aws_ec2_instance_type_offering" "example" {
+  count = length(var.av_zones)
   filter {
     name   = "instance-type"
     values = ["t3.micro"]
   }
   filter {
     name   = "location"
-    values = ["us-east-1a"]
+    values = [var.av_zones[count.index]]
   }
 
   location_type = "availability-zone"
