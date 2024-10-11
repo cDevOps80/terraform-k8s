ff7608ed2979d667b84e49380d619f5132f04fbd
chaithanya1812 <bob@example.com>
2024-10-11 23:13:32 +0530

    terrform-eks module

--- infra-code/eks-module/pod-assocation.tf
+++ infra-code/eks-module/pod-assocation.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_iam_role" "alb_role" {
   name = "alb_role"
 
@@ -23,6 +24,28 @@ resource "aws_iam_role" "alb_role" {
   }
 }
 
+*/
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name               = "eks-pod-identity-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
 
 resource "aws_iam_policy" "alb_policy" {
   name        = "alb_policy"
@@ -239,7 +262,7 @@ resource "aws_iam_policy" "alb_policy" {
 
 resource "aws_iam_role_policy_attachment" "example_s3" {
   policy_arn = aws_iam_policy.alb_policy.arn
-  role       = aws_iam_role.alb_role.name
+  role       = aws_iam_role.example.name
 }
 
 #resource "aws_eks_pod_identity_association" "example" {
