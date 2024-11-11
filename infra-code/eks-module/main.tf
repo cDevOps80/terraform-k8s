6f43b8ef6cb23dca36d0ea0dae65cde1f4467ce3
chaithanya1812 <bob@example.com>
2024-11-11 13:21:54 +0530

    eks ss

--- infra-code/eks-module/main.tf
+++ infra-code/eks-module/main.tf
@@ -33,8 +33,8 @@ resource "aws_eks_node_group" "dev-eks-public-nodegroup" {
   instance_types  = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
